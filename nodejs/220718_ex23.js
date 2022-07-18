d7f63bf0a8cccf1efa086bf6b722d496f2c04d1c
juoklee <juok9802@gmail.com>
2022-07-18 15:22:22 +0900

    study: create node.js module

---
+++ nodejs/220718_ex23.js
@@ -0,0 +1,12 @@
+const os = require("os");
+const path = require("path");
+const filename = __filename;
+
+console.log( "사용가능한 메모리: " + os.freemem() );
+console.log( "전체 메모리 용량: " + os.totalmem() );
+console.log( "홈 디렉토리 경로: " + os.homedir() );
+
+console.log( "경로 구분자: " + path.sep);
+console.log( "현재 파일의 확장자: " + path.extname(filename) );
+console.log( "현재 파일의 경로를 분리해서 출력하기: " );
+console.log( path.parse(filename) );
