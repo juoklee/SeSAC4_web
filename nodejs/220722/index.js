013b2f171836af04b6998ea2599ff338ce47b6fc
juoklee <juok9802@gmail.com>
2022-07-22 15:28:31 +0900

    study: update static, ejs file

--- nodejs/220722/index.js
+++ nodejs/220722/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//view관련파일은 모두 views폴더에 관리
+app.set('view engine', 'ejs');
+
 //'public'이 정적파일을 관리하는 곳이다.
+// 정적파일을 관리하는 가상경로가 /abc
 app.use( '/abc', express.static( 'public' ));
 
 //기본경로 '/'
 app.get('/', (req, res) => {
-    res.send( 'Hello Express!' );
+    res.render("test");
 });
 
 app.listen(port, () => {
