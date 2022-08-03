a8d0f777e5d6d6cb815154ec947c27ffd2a70e58
juoklee <juok9802@gmail.com>
2022-08-03 18:08:28 +0900

    study: update MVC file

--- nodejs/220803/index.js
+++ nodejs/220803/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); 
 
 app.set("view engine", "ejs");
 app.use( express.static( "uploads" ));
 app.use(express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
 
-//기본 =>  ./routes/index 파일
+
+// "/user" 경로에 대해서 router로 가라.
 const router = require("./routes");
-// "/" 경로에 대해서 router로 가라.
+//기본 =>  ./routes/index 파일을 기본으로 한다.
 app.use("/user", router); 
 // "/user" 라는 주제로 묶인 경로로 접속할 시 ( localhost:포트/user/~~~ )
 // ./routes/index.js 에 선언되어 있는 대로 동작한다.
-
-
 
 //ex
 // const board = require("./routes/board");
