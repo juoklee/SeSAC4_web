dc78aee21500bfdc954e90fa99da3f9a28f8cc73
이주옥 <juok9802@gmail.com>
2022-08-09 22:11:42 +0900

    study: update mysql script file, 220805 file

--- nodejs/220805/controller/VisitorController.js
+++ nodejs/220805/controller/VisitorController.js
@@ -1,5 +1,6 @@
 const Visitor = require("../model/Visitor");
 
+//방명록 전체 정보 조회
 exports.get_visitors = (req, res) => {
     Visitor.get_visitors(function( result ) {
         console.log("result: ", result);
@@ -8,6 +9,7 @@ exports.get_visitors = (req, res) => {
     });
 }
 
+//방명록 등록
 exports.post_comment = (req, res) => {
     console.log( req.body );
     
@@ -16,7 +18,7 @@ exports.post_comment = (req, res) => {
     });
 }
 
-
+//방명록 단일 정보 조회
 exports.get_visitor = (req, res) => {
     Visitor.get_visitor(req.query.id, function(result) {
         console.log("result: " , result );
@@ -25,6 +27,7 @@ exports.get_visitor = (req, res) => {
     })    
 }
 
+//방명록 정보 수정
 exports.patch_comment = (req, res) => {
     Visitor.update( req.body, function(result) {
         console.log( result );
@@ -32,6 +35,7 @@ exports.patch_comment = (req, res) => {
     });
 }
 
+//방명록 정보 삭제
 exports.delete_comment = (req, res) => {
     Visitor.delete( req.body.id, function(result) {
         console.log( result );
--- nodejs/220805/model/Visitor.js
+++ nodejs/220805/model/Visitor.js
@@ -7,8 +7,7 @@ const cnn = mysql.createConnection({
     database: 'sesac'
 });
 
-cnn.connect();
-
+//방명록 전체 정보 조회
 exports.get_visitors = (cb) => {
     cnn.query('SELECT * FROM visitor;', (err, rows)=> {
         if ( err ) throw err;
@@ -18,6 +17,7 @@ exports.get_visitors = (cb) => {
     });
 }
 
+//방명록 등록
 exports.insert = ( name, comment, cb ) => {
     var sql = "INSERT INTO visitor (name, comment) VALUES ('" + name + "', '" + comment + "');";
     cnn.query(sql, (err, rows) => {
@@ -28,7 +28,7 @@ exports.insert = ( name, comment, cb ) => {
     });
 }
 
-
+//방명록 단일 정보 조회
 exports.get_visitor = (id, cb) => {
     // id 컬럼의 값이 id인 데이터를 1개만 검색한다.
     cnn.query(`SELECT * FROM visitor Where id= ${id} limit 1;`, (err, rows) => {
@@ -39,7 +39,7 @@ exports.get_visitor = (id, cb) => {
     });
 }
 
-
+//방명록 정보 수정
 exports.update = (data, cb) => {
     var sql = `UPDATE visitor SET name='${data.name}', comment='${data.comment}' WHERE id=${data.id}`;
     cnn.query(sql, (err, rows) => {
@@ -50,6 +50,7 @@ exports.update = (data, cb) => {
     })
 }
 
+//방명록 정보 삭제
 exports.delete = (id, cb) => {
     cnn.query(`DELETE FROM visitor WHERE id=${id}`, (err, rows) => {
         if ( err ) throw err;
