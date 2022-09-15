00d1dc4c880bd6bb3d72a0f90928475264e3af41
이주옥 <juok9802@gmail.com>
2022-09-15 21:54:40 +0900

    study: update socket chat file

--- nodejs/220914/index.js
+++ nodejs/220914/index.js
@@ -13,14 +13,12 @@ app.get("/", function(req, res){
 });
 
 
-// io => 클라이언트와의 모든 연결을 갖고 있는 친구
-// socket => 클라이언트 한명
 var list = {};
 io.on("connection", function(socket){ 
     console.log("connected: ", socket.id);
 
     /* 입장 */
-    socket.on('info2', function(data) {
+    socket.on('info', function(data) {
         list[socket.id] = data.nickname;
         io.emit('notice', data.nickname + "님이 입장하셨습니다.");
         io.emit('list', list);
@@ -31,13 +29,14 @@ io.on("connection", function(socket){
         console.log("client message: ", data.msg);
         data["is_dm"] = false;
         data["nickname"] = list[socket.id];
-        if ( data.to == "전체") {
+        console.log("data:", data);
+        if ( data.to == "Team chat") {
             io.emit("newMessage", data); //모든 클라이언트에게 data 보내기
         } else {
             data["is_dm"] = true;
             let socketID = Object.keys(list).find( (key) => {return list[key] === data.to}); //닉네임이 같을 때의 key(socket.id)
-            io.to(socketID).emit("newMessage", data);
-            socket.emit("newMessage", data);
+            io.to(socketID).emit("newMessage", data); // DM
+            socket.emit("newMessage", data); // 나의 메세지
         }
     });
 
@@ -46,6 +45,7 @@ io.on("connection", function(socket){
         console.log("list[socket.id]:", list[socket.id]);
         io.emit("notice", list[socket.id] + "님이 퇴장하셨습니다."); //퇴장 notice
         delete list[socket.id]; // key,value 둘다 삭제
+        io.emit('list', list);
     });
 });
 
